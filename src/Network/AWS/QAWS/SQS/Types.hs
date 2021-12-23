5a7430241e1fc1c37d458be029b96d7529da43c5
Rickard Andersson <gonz@severnatazvezda.com>
2021-12-23 19:27:03 +0000

    add {From,To}JSON for newtypes

--- src/Network/AWS/QAWS/SQS/Types.hs
+++ src/Network/AWS/QAWS/SQS/Types.hs
@@ -3,35 +3,36 @@
 module Network.AWS.QAWS.SQS.Types where
 
 import Control.Lens.TH (makeLenses)
+import Data.Aeson (FromJSON (..), ToJSON (..))
 import qualified Network.AWS as AWS
 import RIO
 
 newtype QueueUrl = QueueUrl {unQueueUrl :: Text}
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 newtype WaitTime = WaitTime {unWaitTime :: Int}
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 newtype MessageLimit = MessageLimit {unMessageLimit :: Int}
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 newtype ReceiptHandle = ReceiptHandle {unReceiptHandle :: Text}
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 newtype MessageId = MessageId {unMessageId :: Text}
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 newtype ARN = ARN {unARN :: Text}
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 newtype MessageCount = MessageCount {unMessageCount :: Int}
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 newtype DelayedMessageCount = DelayedMessageCount {unDelayedMessageCount :: Int}
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 newtype NotVisibleCount = NotVisibleCount {unNotVisibleCount :: Int}
-  deriving (Eq, Show)
+  deriving (Eq, Show, Generic, FromJSON, ToJSON)
 
 -- | A message that is sent to a queue. In contrast to the standard message type in the amazonka
 -- libraries, this asserts that message id, receipt handle and body are all present.
